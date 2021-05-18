ac0b91ca95e298017a90c5eff1d33a53ba8978c6
NAM NGUYEN <Nam.Nguyen@ibm.com>
2021-05-17 23:48:26 -0400

    End of lecture 142: Express error handling

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from 'express';
+
+const router = express.Router();
+
+router.get('/api/users/currentuser', (req: Request, res: Response) => {
+  res.send('Get Current User!');
+});
+
+export { router as currentUserRouter };
