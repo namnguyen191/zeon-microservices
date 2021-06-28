c08f01a1213f55c4ec2ec2e02669345b06ea65ab
= <=>
2021-06-27 20:52:03 -0400

    End of section 251

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,7 +1,5 @@
+import { currentUser, requireAuth } from '@zeon_dev/common';
 import express, { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
-import { currentUser } from '../middlewares/current-user';
-import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
